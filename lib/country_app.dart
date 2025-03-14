26fed2dac4da6eef55ef04bbdd09c1ca92047331
Mahmud Ahmedov <axmedovmaxmud839@gmail.com>
2025-03-14 14:45:35 +0500

    Loyiha yaratish

---
+++ lib/country_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CountryApp extends StatelessWidget {
+  const CountryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:country_app/country_app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(
+    CountryApp(),
+  );
+}
